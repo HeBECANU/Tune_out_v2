7feb91788edfecd386de834ced2d65662c5a082f
Bryce Henson <bryce.henson@live.com>
2019-02-21 16:48:29 +1100

    fixed error in bryce_stoke_play with the retardation angle to actually be quater wave plate

--- dev/bryce_stokes_play.m
+++ dev/bryce_stokes_play.m
@@ -51,8 +51,9 @@ legend('transmitted intensity','pred min','pred max');
 
 %% ok we have now validated our tools lets compare the min/max power through the analyser for two elipticalites
 rotate_angle=pi/8; %free parameter
-lightin_pcirc=stokes_elip(0.8*pi/2,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
-lightin_ncirc=stokes_elip(-0.8*pi/2,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
+impurity_angle=2*0.07;
+lightin_pcirc=stokes_elip(impurity_angle,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
+lightin_ncirc=stokes_elip(-impurity_angle,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
 light_in={lightin_pcirc,lightin_ncirc};
 I_p_psi_chi=stokes_to_sph(light_in);
 p_min=sin(I_p_psi_chi(:,4)).^2;
@@ -60,24 +61,25 @@ p_max=cos(I_p_psi_chi(:,4)).^2;
 a_max=I_p_psi_chi(:,3);
 a_min=I_p_psi_chi(:,3)+pi/2;
 
-%as expexted there is no measurabld difference
+%as expexted there is no measurabl difference
 
 %%
+
+rotate_angle=pi/2; %free parameter
 impurity_angle=2*0.07;
-rotate_angle=pi/8; %free parameter
 lightin_pcirc=stokes_elip(impurity_angle,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
 lightin_ncirc=stokes_elip(-impurity_angle,rotate_angle); %first argument 0 is pure lin pi/2 is pure circ, second is roatation
 light_lr_circ={lightin_pcirc,lightin_ncirc};
 lr_compare=[];
 for ii=1:2
-light_in=light_lr_circ{ii}
+light_in=light_lr_circ{ii};
 I_p_psi_chi=stokes_to_sph(light_in);
 a_max=I_p_psi_chi(:,3);
 a_min=I_p_psi_chi(:,3)+pi/2;
 %center the qwp rotation on the min or max transmission
 qwp_angles=a_min+linspace(-1,1,1e3)*pi;
 %qwp_angles=a_max+linspace(-1,1,1e3)*pi;
-light_qwp=mueller_rotate(mueller_linretarder(pi/4),qwp_angles);
+light_qwp=mueller_rotate(mueller_linretarder(2*pi/4),qwp_angles);
 lightout=mueller_stokes(light_qwp,light_in);
 
 I_p_psi_chi=stokes_to_sph(lightout);
@@ -97,32 +99,34 @@ figure(46)
 clf
 set(gcf,'color','w')
 
-angle_conv=1/(2*pi)
+angle_conv=1/(2*pi);
+angle_conv_label='/2\pi';
 subplot(2,2,1)
 plot(lr_compare(1).qwp_angles*angle_conv,lr_compare(1).a_max*angle_conv,'r')
 hold on
 plot(lr_compare(2).qwp_angles*angle_conv,lr_compare(2).a_max*angle_conv,'b')
 hold off
-ylabel('max power angle of PBS')
-xlabel('qwp angle')
+ylabel(sprintf('angle of PBS to give max power %s',angle_conv_label))
+xlabel(sprintf('qwp angle %s',angle_conv_label))
 subplot(2,2,2)
 plot(lr_compare(1).qwp_angles*angle_conv,lr_compare(1).a_min*angle_conv,'r')
 hold on
 plot(lr_compare(2).qwp_angles*angle_conv,lr_compare(2).a_min*angle_conv,'b')
 hold off
-ylabel('min power angle of PBS')
+ylabel(sprintf('angle of PBS to give min power %s',angle_conv_label))
+xlabel(sprintf('qwp angle %s',angle_conv_label))
 subplot(2,2,3)
 plot(lr_compare(1).qwp_angles*angle_conv,lr_compare(1).p_max,'r')
 hold on
 plot(lr_compare(2).qwp_angles*angle_conv,lr_compare(2).p_max,'b')
 hold off
 ylabel('max power of PBS')
-xlabel('qwp angle')
+xlabel(sprintf('qwp angle %s',angle_conv_label))
 subplot(2,2,4)
 plot(lr_compare(1).qwp_angles*angle_conv,lr_compare(1).p_min,'r')
 hold on
 plot(lr_compare(2).qwp_angles*angle_conv,lr_compare(2).p_min,'b')
 hold off
 ylabel('min power of PBS')
-xlabel('qwp angle')
+xlabel(sprintf('qwp angle %s',angle_conv_label))
 
