fcca1e1bfc9ec6da19d523c38d95f5dfed2e98ef
Bryce Henson <bryce.henson@live.com>
2019-03-12 12:38:37 +1100

    lots of changes

--- to_hwp_dep.m
+++ to_hwp_dep.m
@@ -247,7 +247,11 @@ polz_power_frac=sqrt(polz_data(:,1)./(polz_data(:,3)));
 %polz_power_frac=2*polz_data(:,1).*polz_data(:,3)./(polz_data(:,1).^2+polz_data(:,3).^2);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 %polz_power_frac=2*sqrt(polz_data(:,1).*polz_data(:,3))./(polz_data(:,1)+polz_data(:,3));
+=======
+polz_power_frac=2*sqrt(polz_data(:,1).*polz_data(:,3))./(polz_data(:,1)+polz_data(:,3));
+>>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
 =======
 polz_power_frac=2*sqrt(polz_data(:,1).*polz_data(:,3))./(polz_data(:,1)+polz_data(:,3));
 >>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
@@ -371,6 +375,9 @@ modelfun = @(b,x) b(1).*(x(:,2).*cos(2.*(x(:,1).*pi./180+b(2).*2*pi)))+b(3);
 modelfun = @(b,x) b(1).*(cos(x(:,1).*pi./180+b(2).*2*pi).^2)+b(3);
 %modelfun = @(b,x) b(1).*(cos(2.*(x(:,1).*pi./180+b(2).*2*pi)))+b(3);
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
+=======
 >>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
 =======
 >>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
@@ -497,8 +504,13 @@ sin_f = @(b,x) sin(b(1))*sqrt(1-b(3)^2).*(cos(x(:,1).*pi./90+b(2).*2*pi))+b(3).*
 beta0=[0.15,0,0];
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 fit_mdl_sin = fitnlm(bc_angles_wraped,-2.*polz_power_frac.*polz_sign,sin_f,beta0,...
     'Options',opts,'CoefficientNames' ,{'amp','phase','offset'});
+=======
+fit_mdl_sin = fitnlm(bc_angles_wraped,-polz_power_frac.*polz_sign,sin_f,beta0,...
+    'Options',opts,'CoefficientNames' ,{'retardance','phase','S_3'});
+>>>>>>> 6de15477018eba4f08a344e5fafd952c48dbe4ed
 =======
 fit_mdl_sin = fitnlm(bc_angles_wraped,-polz_power_frac.*polz_sign,sin_f,beta0,...
     'Options',opts,'CoefficientNames' ,{'retardance','phase','S_3'});
