085ad70c3c7b28f173f4d0038101809673051ab1
Kieran Thomas <kthom796@gmail.com>
2019-03-20 12:04:59 +1100

    updating pol anal

--- to_full_dep.m
+++ to_full_dep.m
@@ -1,157 +1,94 @@
+%% Theory
+to_freq_theory = 725736.480*1e9; %best theory guess in Hz
+to_wav_theory = 299792458./to_freq_theory;
+%% Exp parameters
+aom_freq = 189.*1e6; %aom offset in Hz
 %% HWP
 loop_config.dir = {
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_51_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_29_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190201_to_hwp_131_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_171_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_191_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_208_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\\20190211_to_hwp_194_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_187_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_181_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_177_nuller_reconfig_part_b\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_177_nuller_reconfig_part_a\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_171_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_165_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_160_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_155_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_145_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_140_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190208_to_hwp_120_nuller_reconfig_okish\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190208_to_hwp_99_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190207_to_hwp_80_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_121_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_24_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_46_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_61_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_92_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_230_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_217_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_199_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190206_to_hwp_100_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_141_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_160_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_180_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_70_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_240_nuller_reconfig\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_250_nuller_reconfig_tenma_setpoint\',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190201_to_hwp_111_nuller_reconfig\',
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_51_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_29_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190201_to_hwp_131_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_171_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_191_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_208_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\\20190211_to_hwp_194_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_187_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_181_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_177_nuller_reconfig_part_b\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_177_nuller_reconfig_part_a\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_171_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_165_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190210_to_hwp_160_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_155_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_145_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190209_to_hwp_140_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190208_to_hwp_120_nuller_reconfig_okish\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190208_to_hwp_99_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190207_to_hwp_80_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_121_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_24_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_46_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_61_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_92_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_230_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_217_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_199_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190206_to_hwp_100_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_141_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_160_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_180_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190204_to_hwp_70_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_240_nuller_reconfig\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190211_to_hwp_250_nuller_reconfig_tenma_setpoint\'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190201_to_hwp_111_nuller_reconfig\'
     'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190203_to_hwp_151_nuller_reconfig\'
 };
 data.hwp = to_data(loop_config);
 %% QWP
 loop_config.dir = {
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190304_qwp_280_pure',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190302_qwp_283_pure_long',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190301_qwp_283_pure_run',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190301_qwp_246_2',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_270',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_286',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_310',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_286_no_analog',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_254',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_246',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_234',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_226',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_220',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_202',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_187',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_177',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_162',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_154',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_130',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_134',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_138',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_142',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_146',
-    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190223_qwp_150',
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190304_qwp_280_pure'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190302_qwp_283_pure_long'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190301_qwp_283_pure_run'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190301_qwp_246_2'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_270'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_286'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_310'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190227_qwp_286_no_analog'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_254'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_246'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_234'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_226'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_220'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_202'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_187'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_177'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_162'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190225_qwp_154'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_130'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_134'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_138'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_142'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190224_qwp_146'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190223_qwp_150'
     'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190226_qwp_260'
 };
 data.qwp = to_data(loop_config);
 %% MIX
 loop_config.dir = {
-    'Y:\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\20190305_hwp_340_qwp_270',
-    'Y:\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\20190306_hwp_350_qwp_274',
-    'Y:\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\20190306_overnight_hwp_354_qwp_268',
-    'Y:\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\20190307_hwp_10_qwp_280',
-    'Y:\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\20190307_hwp_06_qwp_290'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190305_hwp_340_qwp_270'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190306_hwp_350_qwp_274'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190306_overnight_hwp_354_qwp_268'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190307_hwp_10_qwp_280'
+    'Z:\EXPERIMENT-DATA\2018_Tune_Out_V2\20190307_hwp_06_qwp_290'
 };
 data.mix = to_data(loop_config);
-%% polarisation data
-%      QWP    HWP      Pmax     Phi max     Pmin     Phi min   hand
-pol_data_post = [
-       NaN    6.0000   75.0000  146.0000    0.9600   50.5000   -1.0000
-       NaN  354.0000   89.0000  120.0000    0.9400   29.0000   -1.0000
-       NaN  314.0000  120.0000   40.0000    0.3700  130.5000    1.0000
-       NaN  335.0000  113.0000   84.0000    0.1300  170.0000   -1.0000
-       NaN  344.0000  133.0000  104.0000    0.6800  190.0000   -1.0000
-       NaN  353.0000   57.0000  298.0000    0.6500  208.0000   -1.0000
-       NaN  347.0000   78.0000  282.0000    0.6600  194.0000   -1.0000
-       NaN  342.0000   92.0000  278.0000    0.5200  187.0000   -1.0000
-       NaN  340.0000   71.0000  271.0000    0.3300  181.0000   -1.0000
-       NaN  338.5000   65.0000  266.0000    0.2300  176.5000   -1.0000
-       NaN  338.5000   65.0000  266.0000    0.2300  176.5000   -1.0000
-       NaN  335.0000   79.0000  264.0000    0.1600  171.0000   -1.0000
-       NaN  332.0000   80.0000  255.0000    0.2000  165.0000    1.0000
-       NaN  319.0000  120.0000   70.5000    0.1200  160.0000    1.0000
-       NaN  327.0000   60.4000  248.0000    0.1100  155.0000    1.0000
-       NaN  324.0000   87.6000  238.0000    0.2300  147.0000    1.0000
-       NaN  320.0000  112.0000   50.0000    0.3300  140.5000    1.0000
-       NaN  309.0000   78.0000  221.0000    0.3700  120.0000    1.0000
-       NaN  299.0000   72.0000  188.0000    0.2500   99.5000    1.0000
-       NaN  290.0000  107.5000  165.0000    0.0700   80.0000   -1.0000
-       NaN  310.0000   86.0000   32.0000    0.3600  121.0000    1.0000
-       NaN  352.0000   72.0000  112.0000    0.5400   24.5000   -1.0000
-       NaN    3.0000   74.0000  137.0000    0.8600   46.0000   -1.0000
-       NaN   10.0000   84.0000  154.0000    0.5200   61.0000   -1.0000
-       NaN   26.0000   77.0000  181.0000    0.0600   92.0000    1.0000
-       NaN    4.0000   70.1000  135.0000    0.7000  231.0000   -1.0000
-       NaN  358.0000   78.0000  308.0000    0.9000  217.0000   -1.0000
-       NaN  350.0000   82.0000  290.0000    0.7200  199.0000   -1.0000
-       NaN  300.0000   72.0000  191.0000    0.1700  100.0000    1.0000
-       NaN  320.0000   62.0000  230.0000    0.2400  140.0000    1.0000
-       NaN  329.0000   76.0000  249.0000    0.1400  160.0000    1.0000
-       NaN  339.0000   93.0000   89.0000    0.1900  180.0000   -1.0000
-       NaN   15.0000  120.0000  165.0000    0.5900   70.0000   -1.0000
-       NaN    9.5000   53.0000  150.0000    0.6400  239.5000   -1.0000
-       NaN   14.5000   75.1000  160.0000    0.5800  249.0000   -1.0000
-       NaN  304.0000   79.0000   20.0000    0.3400  111.0000    1.0000
-       NaN  325.0000   98.0000   63.0000    0.1400  151.0000    1.0000
-  280.0000  333.0000  168.0000  257.0000    0.1300  171.0000    1.0000
-  283.0000  333.0000  195.0000   83.0000    0.6000  352.0000    1.0000
-  283.0000  333.0000  195.0000   83.0000    0.6000  352.0000    1.0000
-  246.0000  333.0000  127.0000   29.0000   45.0000  136.0000   -1.0000
-  270.0000  333.0000  167.3000  248.0000    5.3700  159.0000   -1.0000
-  286.5000  333.0000  165.0000   88.5000    2.1800  179.0000    1.0000
-  310.0000  333.0000  140.7000  117.0000   40.8000  205.0000    1.0000
-  286.0000  333.0000  165.0000   88.5000    2.1800  179.0000    1.0000
-  254.0000  333.0000  115.0000  240.0000   22.1000  143.0000   -1.0000
-  246.0000  333.0000  101.0000   50.0000   42.0000  316.0000   -1.0000
-  234.0000  333.0000   95.0000  226.0000   82.0000  319.0000   -1.0000
-  226.0000  333.0000   87.0000  290.0000   73.0000  199.0000   -1.0000
-  220.0000  333.0000  110.0000  273.0000   44.1000  192.0000   -1.0000
-  202.0000  333.0000  178.0000  262.0000   11.5000  355.0000   -1.0000
-  187.0000  333.0000  165.0000  253.0000    0.4600  343.0000    1.0000
-  177.0000  333.0000  150.0000  248.0000    8.7000  338.0000    1.0000
-  162.0000  333.0000  132.0000  242.0000   35.0000  329.0000    1.0000
-  154.0000  333.0000  113.0000  236.0000   58.0000  328.0000    1.0000
-  130.0000  333.0000  119.0000  284.0000   31.0000   12.0000    1.0000
-  134.0000  333.0000  156.0000  292.0000   59.0000   18.0000    1.0000
-  138.0000  333.0000  134.0000  284.0000   58.0000    3.0000    1.0000
-  142.0000  333.0000   77.5000  278.0000   53.0000    9.0000    1.0000
-  146.0000  333.0000  118.0000  260.0000   74.0000  344.0000    1.0000
-  150.0000  333.0000   90.6000  245.0000   63.9000  152.0000    1.0000
-  260.0000  333.0000  124.0000   51.0000   18.1000  329.0000   -1.0000
-  270.0000  340.0000  206.5000   66.0000   34.4000  156.0000   -1.0000
-  274.0000  350.0000  205.0000  222.0000   143.200  314.0000   -1.0000
-  268.0000  354.0000  176.0000  165.0000    87.000  252.0000   -1.0000
-  280.0000  10.0000   148.0000  352.0000    11.200   83.0000   -1.0000
-  290.0000   6.0000   123.5000   12.0000    49.100   281.000   -1.0000
-  ];
+%% polarisation data options
+pol_opts.location = 'post';
+pol_opts.predict = 'obs';%'interp'; %options measure (what we measured) or interpolated (create model based on all measured values)
 %% Display options
 opts = statset('MaxIter',1e4);
 ci_size=1-erf(1/sqrt(2));%1-erf(zvalue/sqrt(2)) %confidence interval for cutting outliers
-disp_config.font_name = 'cmr10';
+disp_config.font_name = 'Times New Roman';%'cmr10';
 disp_config.font_size_global=14;
 disp_config.opts=statset('nlinfit');
 disp_config.colors_main = [[75,151,201];[193,114,66];[87,157,95]];
@@ -168,10 +105,7 @@ V = zeros(numel(to_val),1);%fourth stokes parameter
 d_p = zeros(numel(to_val),1);%power diffrence between min and max transmition
 theta = zeros(numel(to_val),1);%min angle
 
-V_run = pol_data_post(:,7).*2.*sqrt(pol_data_post(:,3).*pol_data_post(:,5))...
-    ./(pol_data_post(:,3)+pol_data_post(:,5));%the V parameter for each run
-d_p_run = (pol_data_post(:,5)-pol_data_post(:,3))./(pol_data_post(:,3)+pol_data_post(:,5));
-theta_run = pol_data_post(:,6).*pi/180; %using min pow angle
+[V_run,d_p_run,theta_run] = pol_data_import(pol_opts);
 
 shot_idx = 1;
 scan_num_vec = [data.hwp.main.scan_num;data.qwp.main.scan_num;data.mix.main.scan_num];
@@ -190,7 +124,7 @@ full_mdl = @(b,x) b(1) + b(2).*x(:,2) + b(3).*(1+Q_fun(x(:,1),x(:,3),b(4)));%ful
 vars = [d_p,V,theta];
 to_val_fit = to_val;
 wlin_fit = wlin./sum(wlin);
-beta0 = [7.257355*1e14,6*1e9,3*1e9,0.8];
+beta0 = [7.257355*1e14,6*1e9,3*1e9,0.2];
 fit_mdl = fitnlm(vars,to_val_fit,full_mdl,beta0,...
     'Options',opts,'Weights',wlin_fit,'CoefficientNames' ,{'tune_out','vector','tensor','phase'});
 fit_vals = fit_mdl.Coefficients{:,1};
@@ -263,3 +197,17 @@ sfigure(4445005);
 histfit(to_res./1e6,50)
 xlabel('residual (MHz)')
 ylabel('count')
+%% Write out the results
+
+to_freq_val=fit_vals(1)+aom_freq;
+to_freq_unc=fit_uncs(1);
+to_wav_val=299792458/(to_freq_val);
+to_wav_unc=2*to_freq_unc*299792458/(to_freq_val^2);
+old_to_wav=413.0938e-9;
+
+fprintf('\n====TO full fit results==========\n')
+fprintf('TO freq             %.1f±(%.0f) MHz\n',...
+    to_freq_val*1e-6,to_freq_unc*1e-6)
+fprintf('TO wavelength       %.6f±%f nm \n',to_wav_val*1e9,to_wav_unc*1e9)
+fprintf('diff from TOV1               %e±%e nm \n',(to_wav_val-old_to_wav)*1e9,to_wav_unc*1e9)
+fprintf('diff from Theory             %e±%e nm \n',(to_wav_val-to_wav_theory)*1e9,to_wav_unc*1e9)
