09e6cb0567e57ec6d222f2d4f4dec128ddb3e718
bryce <bryce.henson@live.com>
2019-06-04 13:30:57 +1000

    fixed bug in checking if folder has been modified

--- lib/should_process_folder.m
+++ lib/should_process_folder.m
@@ -1,5 +1,6 @@
 function process_folder=should_process_folder(import_dir,reprocess_folder_if_older_than,active_process_mod_time)
 
+
 process_folder=false;
 currently_processing=false;
 has_valid_done=false;
@@ -11,13 +12,13 @@ if exist(out_dir_folder,'dir')
     % check that the process time is newer than reprocess_folder_if_older_than
     out_sub_folders=dir(out_dir_folder);
     out_sub_folders=out_sub_folders(3:end);
-    out_sub_folders=out_sub_folders(out_sub_folders.isdir);
+    out_sub_folders=out_sub_folders(cat(1,out_sub_folders.isdir));
     times_posix=cellfun(@(x) posixtime(datetime(datenum(x,'yyyymmddTHHMMSS'),'TimeZone','local','ConvertFrom','datenum')),{out_sub_folders.name});
     newer_than_reprocess_time=times_posix>reprocess_folder_if_older_than;
     if sum(newer_than_reprocess_time)==0
         process_folder=true;
     else
-        %check that that out dir dose not have a mod time that is close enough to the current date
+        %check that that out dir does not have a mod time that is close enough to the current date
         up_to_date_subfolders=out_sub_folders(newer_than_reprocess_time);
         for ii=1:numel(up_to_date_subfolders)
             out_sub_folder_contents=dir(fullfile(out_dir_folder,up_to_date_subfolders(ii).name));
